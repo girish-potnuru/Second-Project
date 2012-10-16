635b902297714f1843d6ed32278abf715276129c
Prasad <prasadb@Prasad-Bs-Mac-mini.local>
2012-10-16 12:41:34 +0530

    added by girish

--- SQLiteTutorial/SQLiteTutorial/AppDelegate.m
+++ SQLiteTutorial/SQLiteTutorial/AppDelegate.m
@@ -29,6 +29,7 @@
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+    //This is updated by girish
     return YES;
 }
 
